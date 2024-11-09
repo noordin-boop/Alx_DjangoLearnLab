6ffb51d1f022f5c97c27d7ee0922549c1bf3437e
Noordin <noordinali156@gmail.com>
2024-11-09 21:17:24 +0300

    Add function-based and class-based views with templates and URL routing

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from . import views
-
+from .views import list_books
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # URL for function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
