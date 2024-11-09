542773b56994f281bc0c7d115403aed819765ebc
Noordin <noordinali156@gmail.com>
2024-11-09 20:33:40 +0300

    Initial commit for django-models  project with filter labir

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,14 +3,14 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)  # Here, we use filter to retrieve all books by this author
+    return Book.objects.filter(author=author)
 
 # List all books in a library
 def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()  # This retrieves all books in the specified library
+    return library.books.all()
 
-# Retrieve the librarian for a library
+# Retrieve the librarian for a library using Librarian.objects.get
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian  # Retrieves the one-to-one librarian for the specified library
+    return Librarian.objects.get(library=library)  # Using Librarian.objects.get with library filter
