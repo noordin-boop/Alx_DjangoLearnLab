fd8832c9b0d0b9a3f93a8c848d6831b381bfd880
Noordin <noordinali156@gmail.com>
2024-11-24 17:59:09 +0300

    Implement CRUD operations using ViewSets and Routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,8 +2,14 @@ from rest_framework.generics import ListAPIView  # Ensure this is imported
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 
 # A view to list all books using DRF's generic ListAPIView
 class BookList(generics.ListAPIView):  # Ensure this inherits from ListAPIView
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
