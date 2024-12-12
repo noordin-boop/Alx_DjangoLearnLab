ad54252a07850a1443e7f6103102ac8318d669c6
Noordin <noordinali156@gmail.com>
2024-12-12 19:02:54 +0300

    Initial setup for django_blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',          # Your database name
+        'USER': 'root',          # Your MySQL username
+        'PASSWORD': 'password',  # Your MySQL password
+        'HOST': '127.0.0.1',     # Default MySQL host
+        'PORT': '3306',          # Default MySQL port
     }
 }
 
